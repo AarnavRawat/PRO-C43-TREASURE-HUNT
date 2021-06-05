0b2bad8a8f32605a4519fec2f3b093ad53fd9433
AarnavR <72977107+AarnavRawat@users.noreply.github.com>
2021-06-05 11:42:51 +0530

    Add files via upload
    making the changes on the end Game

--- js/Security.js
+++ js/Security.js
@@ -10,7 +10,7 @@ class Security {
         this.button1.position(100,120);
         this.button1.style('background', 'lightgrey');    
 
-        this.access2 = createInput("Code1")
+        this.access2 = createInput("Code2")
         this.access2.position(700,190);
         this.access2.style('background', 'white');  
 
@@ -18,7 +18,7 @@ class Security {
         this.button2.position(700,220);
         this.button2.style('background', 'lightgrey');
 
-        this.access3 = createInput("Code1")
+        this.access3 = createInput("Code3");
         this.access3.position(100,290);
         this.access3.style('background', 'white');  
 
@@ -33,6 +33,8 @@ class Security {
 
         this.button1.mousePressed(() => {
             if(system.authenticate(accessCode1,this.access1.value())){
+                console.log("accessCode1: "+accessCode1);
+                console.log("this.access1.value(): "+this.access1.value());
                 this.button1.hide();
                 this.access1.hide();
                 score++;
@@ -41,6 +43,8 @@ class Security {
 
         this.button2.mousePressed(() => {
             if(system.authenticate(accessCode2,this.access2.value())){
+                console.log("accessCode2: "+accessCode2);
+                console.log("this.access2.value(): "+this.access2.value());
                 this.button2.hide();
                 this.access2.hide();
                 score++;
@@ -49,6 +53,8 @@ class Security {
 
         this.button3.mousePressed(() => {
             if(system.authenticate(accessCode3,this.access3.value())){
+                console.log("accessCode3: "+accessCode3);
+                console.log("this.access3.value(): "+this.access3.value());
                 this.button3.hide();
                 this.access3.hide();
                 score++;
--- sketch.js
+++ sketch.js
@@ -1,21 +1,26 @@
-var bg,bg2,form,system,code,security;
-var score=0;
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+var bg, bg2, form, system, code, security;
+var score = 0;
 
 function preload() {
- 
-  
-  bg= loadImage("aladdin_cave.jpg")
+
+
+  bg = loadImage("aladdin_cave.jpg");
   //load image for the treasure background
-  
+  bg2 = loadImage("treasure.jpg");
+
 }
 
 function setup() {
-  createCanvas(1000,500);
+  createCanvas(1000, 500);
   security = new Security();
   system = new System();
-  
-  
-  
+
+
+
 }
 
 function draw() {
@@ -25,16 +30,15 @@ function draw() {
   textSize(20);
   fill("white");
   text("Score: " + score, 450, 50);
-// add code for changing the background to the treasure background
   
-
-  if(score === 3) {
-    clear()
-    background(bg2)
-    fill("black")
+  // add code for changing the background to the treasure background
+  if (score === 3) {
+    clear();
+    background(bg2);
+    fill("black");
     textSize(40);
-    text("TREASURE UNLOCKED",250, 200);
+    text("TREASURE UNLOCKED", 250, 200);
   }
 
-  drawSprites()
+  drawSprites();
 }
